9179539cfaf65a9239dbe4173a8e188beb5d2ea5
Bhaskar Panja <bhaskarpanja93@gmail.com>
2022-05-13 11:36:06 +0530

    Server side: >> No new cursor variable created for database. Faster database operations.

--- extra_py_files/ADFLY_host.py
+++ extra_py_files/ADFLY_host.py
@@ -144,8 +144,7 @@ def user_login_manager(connection, address):
         for reserved_word in reserved_u_names_words:
             if reserved_word in u_name:
                 return False
-        db_cursor = db_connection.cursor()
-        all_u_names = [row[0] for row in db_cursor.execute("SELECT u_name from user_data")]
+        all_u_names = [row[0] for row in db_connection.cursor().execute("SELECT u_name from user_data")]
         if u_name in all_u_names:
             return False
         else:
@@ -176,8 +175,7 @@ def user_login_manager(connection, address):
             if response_code == '0': # login u_name
                 login_success = False
                 u_name = __receive_from_connection(connection).decode().strip().lower()
-                db_cursor = db_connection.cursor()
-                all_u_names = [row[0] for row in db_cursor.execute("SELECT u_name from user_data")]
+                all_u_names = [row[0] for row in db_connection.cursor().execute("SELECT u_name from user_data")]
                 if u_name in all_u_names:
                     __send_to_connection(connection, b'0')
                     login_possible = True
@@ -195,8 +193,7 @@ def user_login_manager(connection, address):
                 login_success = False
                 if u_name and login_possible:
                     password = __receive_from_connection(connection).decode().strip().swapcase()
-                    db_cursor = db_connection.cursor()
-                    user_pw_hash = [_ for _ in db_cursor.execute(f"SELECT user_pw_hash from user_data where u_name = '{u_name}'")][0][0]
+                    user_pw_hash = [_ for _ in db_connection.cursor().execute(f"SELECT user_pw_hash from user_data where u_name = '{u_name}'")][0][0]
                     if check_password_hash(user_pw_hash, password):
                         __send_to_connection(connection, b'0')
                         login_success = True
@@ -206,8 +203,7 @@ def user_login_manager(connection, address):
                     password = __receive_from_connection(connection).decode().strip().swapcase()
                     if password_matches_standard(password):
                         user_pw_hash = generate_password_hash(password, salt_length=1000)
-                        db_cursor = db_connection.cursor()
-                        db_cursor.execute(f"INSERT into user_data (u_name, user_pw_hash, instance_token) values ('{u_name}', '{user_pw_hash}', '{generate_random_string(1000,5000)}')")
+                        db_connection.cursor().execute(f"INSERT into user_data (u_name, user_pw_hash, instance_token) values ('{u_name}', '{user_pw_hash}', '{generate_random_string(1000,5000)}')")
                         db_connection.commit()
                         __send_to_connection(connection, b'0')
                         login_success = True
@@ -217,8 +213,7 @@ def user_login_manager(connection, address):
                     __send_to_connection(connection, b'-2')
             elif response_code == '3': # add new adfly id
                 if u_name and login_success:
-                    db_cursor = db_connection.cursor()
-                    old_ids = [_ for _ in db_cursor.execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
+                    old_ids = [_ for _ in db_connection.cursor().execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
                     if not old_ids:
                         old_ids = ""
                     old_ids = old_ids[0][0]
@@ -232,16 +227,14 @@ def user_login_manager(connection, address):
                         __send_to_connection(connection, b'-2')
                     elif new_id not in old_ids.split():
                         old_ids += ' ' + new_id
-                        db_cursor = db_connection.cursor()
-                        db_cursor.execute(f"UPDATE user_data set self_adfly_ids='{old_ids.strip()}' where u_name='{u_name}'")
+                        db_connection.cursor().execute(f"UPDATE user_data set self_adfly_ids='{old_ids.strip()}' where u_name='{u_name}'")
                         db_connection.commit()
                         __send_to_connection(connection, b'0')
                     else:
                         __send_to_connection(connection, b'-1')
             elif response_code == '4': # remove old adfly id
                 if u_name and login_success:
-                    db_cursor = db_connection.cursor()
-                    old_ids = [_ for _ in db_cursor.execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
+                    old_ids = [_ for _ in db_connection.cursor().execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
                     if not old_ids:
                         __send_to_connection(connection, b'-1')
                     else:
@@ -259,8 +252,7 @@ def user_login_manager(connection, address):
                             new_string = ''
                             for _id in old_ids:
                                 new_string += ' ' + _id
-                            db_cursor = db_connection.cursor()
-                            db_cursor.execute(f"UPDATE user_data set self_adfly_ids='{new_string.strip()}' where u_name='{u_name}'")
+                            db_connection.cursor().execute(f"UPDATE user_data set self_adfly_ids='{new_string.strip()}' where u_name='{u_name}'")
                             db_connection.commit()
                             __send_to_connection(connection, b'0')
                         else:
@@ -270,8 +262,7 @@ def user_login_manager(connection, address):
                     __send_to_connection(connection, b'0')
                     password = __receive_from_connection(connection).decode().strip().swapcase()
                     if password_matches_standard(password):
-                        db_cursor = db_connection.cursor()
-                        db_cursor.execute(f"UPDATE user_data SET user_pw_hash='{generate_password_hash(password, salt_length=1000)}', instance_token='{generate_random_string(1000,5000)}' where u_name='{u_name}'")
+                        db_connection.cursor().execute(f"UPDATE user_data SET user_pw_hash='{generate_password_hash(password, salt_length=1000)}', instance_token='{generate_random_string(1000,5000)}' where u_name='{u_name}'")
                         db_connection.commit()
                         __send_to_connection(connection, b'0')
                     else:
@@ -345,8 +336,7 @@ def accept_connections_from_users(port):
                 f.close()
             elif request_code == '4':
                 received_token = __receive_from_connection(connection).decode()
-                db_cursor = db_connection.cursor()
-                all_u_name = [row[0] for row in db_cursor.execute(f"SELECT u_name from user_data where instance_token='{received_token}'")]
+                all_u_name = [row[0] for row in db_connection.cursor().execute(f"SELECT u_name from user_data where instance_token='{received_token}'")]
                 if all_u_name and all_u_name[0]:
                     u_name = all_u_name[0]
                 else:
@@ -384,8 +374,7 @@ def accept_connections_from_users(port):
             elif request_code == '98':
                 received_token = __receive_from_connection(connection).decode().strip()
                 all_u_name = []
-                db_cursor = db_connection.cursor()
-                for row in db_cursor.execute(f"SELECT u_name from user_data where instance_token='{received_token}'"):
+                for row in db_connection.cursor().execute(f"SELECT u_name from user_data where instance_token='{received_token}'"):
                     all_u_name.append(row[0])
                 if all_u_name and all_u_name[0]:
                     u_name = all_u_name[0]
@@ -398,15 +387,12 @@ def accept_connections_from_users(port):
             elif request_code == '99':
                 u_name = __receive_from_connection(connection).decode().strip()
                 password = __receive_from_connection(connection).decode().strip().swapcase()
-                db_cursor = db_connection.cursor()
-                all_u_names = [row[0] for row in db_cursor.execute("SELECT u_name from user_data")]
+                all_u_names = [row[0] for row in db_connection.cursor().execute("SELECT u_name from user_data")]
                 if u_name in all_u_names:
-                    db_cursor = db_connection.cursor()
-                    user_pw_hash = [_ for _ in db_cursor.execute(f"SELECT user_pw_hash from user_data where u_name = '{u_name}'")][0][0]
+                    user_pw_hash = [_ for _ in db_connection.cursor().execute(f"SELECT user_pw_hash from user_data where u_name = '{u_name}'")][0][0]
                     if check_password_hash(user_pw_hash, password):
                         __send_to_connection(connection, b'0')
-                        db_cursor = db_connection.cursor()
-                        instance_token = [row[0] for row in db_cursor.execute(f"SELECT instance_token from user_data where u_name='{u_name}'")][0]
+                        instance_token = [row[0] for row in db_connection.cursor().execute(f"SELECT instance_token from user_data where u_name='{u_name}'")][0]
                         __send_to_connection(connection, instance_token.encode())
                     else:
                         __send_to_connection(connection, b'-1')
@@ -417,8 +403,7 @@ def accept_connections_from_users(port):
                 all_u_name = []
                 try:
                     all_u_name = []
-                    db_cursor = db_connection.cursor()
-                    for row in db_cursor.execute(f"SELECT u_name from user_data where instance_token='{received_token}'"):
+                    for row in db_connection.cursor().execute(f"SELECT u_name from user_data where instance_token='{received_token}'"):
                         all_u_name.append(row[0])
                 except Exception as e:
                     debug_host(str(request_code) + repr(e))
@@ -458,8 +443,6 @@ def update_vm_responses():
                         break
                 except:
                     pass
-
-
     def receive_data(u_name):
         try:
             token = generate_random_string(5, 10)
@@ -476,16 +459,16 @@ def update_vm_responses():
                     break
         except:
             Thread(target=check_and_remove_active_user, args=(u_name,)).start()
-
     def send_blank_command(u_name):
         try:
             __send_to_connection(vm_data_update_connections[u_name], b"x")
         except:
             pass
 
+
     while True:
         for turbo_app in all_turbo_apps:
-            if turbo_app.clients:
+            if turbo_app.clients and sorted([viewer in viewer_credits for viewer in turbo_app.clients])[-1] and sorted([viewer_credits[viewer] for viewer in turbo_app.clients])[-1]:
                 try:
                     current_vm_response_data = {}
                     targets = sorted(vm_data_update_connections)
@@ -661,16 +644,14 @@ def flask_operations(port):
     @app.route('/adf_link_click/', methods=['GET'])
     def adf_link_click():
         u_name = request.args.get('u_name')
-        db_cursor = db_connection.cursor()
-        all_u_names = [row[0] for row in db_cursor.execute("SELECT u_name from user_data")]
-        u_name_ids = [row[0].split() for row in db_cursor.execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
+        all_u_names = [row[0] for row in db_connection.cursor().execute("SELECT u_name from user_data")]
+        u_name_ids = [row[0].split() for row in db_connection.cursor().execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
         if u_name in all_u_names and u_name_ids and u_name_ids[0]:
             id_to_serve = choice(u_name_ids[0])
         else:
             while True:
                 u_name = choice(all_u_names)
-                db_cursor = db_connection.cursor()
-                u_name_ids = [row[0].split() for row in db_cursor.execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
+                u_name_ids = [row[0].split() for row in db_connection.cursor().execute(f"SELECT self_adfly_ids from user_data where u_name = '{u_name}'")]
                 if u_name_ids and u_name_ids[0]:
                     id_to_serve = choice(u_name_ids[0])
                     break
