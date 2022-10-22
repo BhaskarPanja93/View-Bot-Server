7fdd01e27f2379c1a2a426cb9df03b513f8e0651
Bhaskar Panja <bhaskarpanja93@gmail.com>
2022-10-22 17:16:53 +0530

    [ADDED] Complete documentation for proxy_checker.py
    [INCOMPLETE] Threats
    
    [INCOMPLETE] Linkvertise link page added, not finished yet.

--- v3.0/py_files/common/proxy_checker/proxy_checker.py
+++ v3.0/py_files/common/proxy_checker/proxy_checker.py
@@ -14,6 +14,11 @@ from requests import get
 
 
 def verify_global_site():
+    """
+    Sends a cURL ping request to current global host(cURL because proxies don't interact for cURLs), if ping succeeds, the host url is correct, else fetch new url from github
+    :return: None
+    """
+
     global global_host_page
     while True:
         try:
@@ -23,7 +28,6 @@ def verify_global_site():
                 raise ZeroDivisionError
         except:
             try:
-                print("Global host ping failed. Rechecking from github...")
                 text = popen('curl -L -s "https://bhaskarpanja93.github.io/AllLinks.github.io/"').read().split('<p>')[-1].split('</p>')[0].replace('‘', '"').replace('’', '"').replace('“', '"').replace('”', '"').replace("â€˜", "'").replace("â€™", "'")
                 link_dict = eval(text)
                 global_host_page = choice(link_dict['adfly_host_page_list'])
@@ -33,6 +37,11 @@ def verify_global_site():
 
 
 def report_working_proxy(proxy, current_ip):
+    """
+    sends IP and proxy data to server API to add it as a working proxy
+    :return: None
+    """
+
     try:
         system_caller(f'curl -L -s "{global_host_page}/proxy_report?proxy={proxy}&status=working&ip={current_ip}" --max-time 10')
     except:
@@ -40,6 +49,11 @@ def report_working_proxy(proxy, current_ip):
 
 
 def report_failed_proxy(proxy):
+    """
+    sends Proxy data to server API to add it as a failed proxy
+    :return: None
+    """
+
     try:
         system_caller(f'curl -L -s "{global_host_page}/proxy_report?proxy={proxy}&status=failed" --max-time 10')
     except:
@@ -47,6 +61,11 @@ def report_failed_proxy(proxy):
 
 
 def __connect_proxy():
+    """
+    Fetch am unchecked proxy from server APi, add it to system, and restart internet explorer to apply the proxy settings
+    :return: None or Bool if server has no proxy ready
+    """
+
     global current_proxy
     while True:
         current_proxy = ''
@@ -56,7 +75,6 @@ def __connect_proxy():
             # current_proxy = "8.219.97.248:80"
             if current_proxy == '':
                 return False
-            print(f"proxy to connect: {current_proxy}")
             system_caller('reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f')
             system_caller(f'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d {current_proxy} /f')
             restart_ie()
@@ -67,6 +85,11 @@ def __connect_proxy():
 
 
 def restart_ie():
+    """
+    Start and close Internet Explorer
+    :return: None
+    """
+
     sleep(0.1)
     Thread(target=system_caller, args=(' "C:\\Program Files\\internet explorer\\iexplore.exe" ',)).start()
     sleep(2)
@@ -74,11 +97,21 @@ def restart_ie():
 
 
 def __disconnect_proxy():
+    """
+    Removes all proxy from system and restarts Internet Explorer
+    :return: None
+    """
     system_caller('reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f')
     restart_ie()
 
 
 def __get_global_ip():
+    """
+    Asks for Global IP from server APi
+    If connection fails, change url from secure(https) to insecure(http) and retry
+    :return: None
+    """
+
     global global_host_page
     verify_global_site()
     for _ in range(2):
@@ -108,13 +141,17 @@ def __cloudflare_check():
 
 
 def restart_vpn_recheck_ip(_=0):
+    """
+    checks for self IP after adding a proxy, if IP changed, report it as working proxy, else give another try, if still fails, report as failed proxy
+    :return: None
+    """
+
     global last_ip, connection_enabled, current_ip, current_proxy
     __disconnect_proxy()
     sleep(2)
     while True:
         sleep(0.1)
         current_ip = __get_global_ip()
-        print(f"{current_ip=}")
         if (not current_ip) and _:
             if _ < 2:
                 sleep(1)
@@ -149,5 +186,6 @@ while not genuine_ip:
 
 def run():
     while True:
-        print('\n\n\n\nTriggered new restart_vpn_recheck_ip')
+        system_caller('cls')
+        print("Triggered restart_vpn_recheck_ip")
         restart_vpn_recheck_ip(1)
